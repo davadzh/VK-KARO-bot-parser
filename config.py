eb7bf6d79ebbd9ea718e7cb921ed650e4c5d6a06
davidadzaman <adzhamyand@gmail.com>
2019-12-22 22:09:33 +0300

    commit

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 TOKEN = '44e2fe17a91a77f40ecb4d2e15a73457bb3d5d17e9f088699a93909dfe18a94f1ff50a79c23ef406a132d'
 
-HEROKU_URL = 'https://vk-bot-adzhamyan.herokuapp.com/'
+HEROKU_URL = 'https://vk-bot-adj.herokuapp.com/'
